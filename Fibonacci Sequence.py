ece28c4c5caa705e65c57511ea005ea404003f84
Rajan Gautam <gautamrajan073@gmail.com>
2020-11-04 15:37:51 +0530

    One More Program Added

--- Fibonacci Sequence.py
+++ Fibonacci Sequence.py
@@ -1,1 +1,20 @@
 # Fibonacci Sequence
+
+print("Fibonacci Series in Python")
+terms = int(input("Enter Number of Terms: "))
+
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def main():
+    for i in range(terms):
+        print(fibonacci(i))
+
+
+print("The Series is:")
+main()
